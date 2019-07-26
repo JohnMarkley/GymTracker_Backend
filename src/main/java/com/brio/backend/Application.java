90c4b2eabdccd67ee323707e617765f4eb6334ed
John Markley <Johnwilliammarkley@gmail.com>
2019-07-26 09:35:41 -0600

    Final commit before Internship project due

--- src/main/java/com/brio/backend/Application.java
+++ src/main/java/com/brio/backend/Application.java
@@ -27,14 +27,17 @@ public class Application {
             //TODO
             //REPLACE WITH PARSING FROM TEXT FILE
 
-            trafficReposity.save(new Traffic("M", "Less Than Normal", 1, 063000, true));
-            trafficReposity.save(new Traffic("M", "Empty", 1, 120000, true));
+            trafficReposity.save(new Traffic("M", "The Gym Is Less Busy Than Normal", 5, 063000, true));
+            trafficReposity.save(new Traffic("M", "The Gym Will Be Busy On Wednesday, July 31 @ 5:30pm", 15, 063000, true));
+            trafficReposity.save(new Traffic("M", "The Gym Will Be Empty On Monday, August 5 @ 7:30am", 2 , 063000, true));
+
+            trafficReposity.save(new Traffic("M", "Empty", 2, 120000, true));
             trafficReposity.save(new Traffic("M", "Full", 49, 18000, true));
             trafficReposity.save(new Traffic("T", "Busy", 18, 063000, true));
-            trafficReposity.save(new Traffic("T", "Empty", 0, 120000, true));
-            trafficReposity.save(new Traffic("T", "Less Than Normal", 1, 180000, true));
+            trafficReposity.save(new Traffic("T", "Empty", 3, 120000, true));
+            trafficReposity.save(new Traffic("T", "Less Than Normal", 3, 180000, true));
             trafficReposity.save(new Traffic("W", "Packed", 20, 063000, true));
-            trafficReposity.save(new Traffic("W", "Empty", 1, 120000, true));
+            trafficReposity.save(new Traffic("W", "Empty", 2, 120000, true));
             trafficReposity.save(new Traffic("W", "Packed", 20, 180000, true));
 
         };
